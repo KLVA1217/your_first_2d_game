1c1dd2d6d2f58cec95d19ed0c79d804694121ef0
KLVA1217 <kenleonard2010@yahoo.com>
2024-11-04 13:53:40 -0800

    Implemented Exit Button

--- hud.gd
+++ hud.gd
@@ -26,13 +26,18 @@ func show_game_over():
 	# Make a one-shot timer and wait for it to finish.
 	await get_tree().create_timer(1.0).timeout
 	$StartButton.show()
+	$ExitButton.show()
 	
 func update_score(score):
 	$ScoreLabel.text = str(score)
 
 func _on_start_button_pressed() -> void:
 	$StartButton.hide()
+	$ExitButton.hide()
 	start_game.emit()
 
 func _on_message_timer_timeout() -> void:
 	$Message.hide()
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
