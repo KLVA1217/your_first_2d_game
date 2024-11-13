004c50aaab72787c1715e929e739c0e00de54738
KLVA1217 <kenleonard2010@yahoo.com>
2024-11-13 07:51:10 -0800

    Mob animation fits with direction

--- mob.gd
+++ mob.gd
@@ -6,10 +6,19 @@ func _ready() -> void:
 	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
 	
+	var velocity = self.linear_velocity
+	
+	# print("velocity.x < 0 = " + str(velocity.x < 0))
+	$AnimatedSprite2D.flip_h = velocity.x < 0
+	
+	# print("velocity.y < 0 = " + str(velocity.y < 0))
+	$AnimatedSprite2D.flip_v = velocity.y > 0
+	
+	$AnimatedSprite2D.play()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
